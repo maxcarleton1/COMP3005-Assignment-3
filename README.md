bc32fa4510785766a807382a7e1018fd42e350a3
maxcarleton1 <maxsobota@cmail.carleton.ca>
2025-11-02 20:45:34 -0500

    Completed necessary functions, updated README

--- README.md
+++ README.md
@@ -1,11 +1,38 @@
 # COMP3005 Assignment 3
 ### By Max Sobota
+
+## Summary
 Application which connects to a PostgreSQL to perform CRUD (Create, Read, Update, Delete) operations.
 
+## Video
+(Video link here)
+
+## Requirements
 I chose Python as my language of choice to keep things simple, as there are many available libraries to use which make connections to a PostgreSQL database.
-I'm running Python version 3.???.???, and the latest pgAdmin 4 update.
+I'm running Python version 3.11.9.
 
 I used the psycopg2 package for my implementation, to install using pip, run the command "pip install psycopg2". 
 The library can be found here: https://pypi.org/project/psycopg2/
 
+## Running
+Run with "python application.py"
+
+## Query
+Here is the query I used to create the database:
+
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    enrollment_date DATE
+)
+
+I then populated it using the query given on the assignment spec.
+Since it wasn't clearly stated, I assumed the database already exists and is populated using these queries prior to running the application.py script.
+
+## Assumptions and other details
 I'm using a 255 character limit for the strings as we've previously used in our SQL assignments.
+I wasn't certain how important user interaction was, so I just went for a simple terminal interface.
+For addStudent, I assumed that enrollment_date would be a string in the correct format "year-month-date", as that's how psycopg2 processes them.
+Seeing as psycopg2 requires a connection to the database to run queries, and I couldn't edit the function signatures, I opted to just make a new connection for each function.
--- application.py
+++ application.py
@@ -1,89 +1,143 @@
 # Name: Max Sobota
 
-#import psycopg2 # The library which allows connecting to PostgreSQL databases
-from datetime import datetime # Allows entering date objects
+import psycopg2 # The library which allows connecting to PostgreSQL databases
+
+# Questions: 
+# Do we need to validate data? Example: enrollment_date
+# Do we need a UI, or just implementing the functions?
+# Do we need CHAR_LIMIT?
+# Can I alter the function signatures to pass in the connection/cursor?
+# Office hours: monday 3-4pm HP 4125
 
 CHAR_LIMIT = 255
 
+connection = psycopg2.connect(database="assignment3q1", host="localhost", user="postgres", password="password", port="5432") # For testing
+cursor = connection.cursor()
+
 def start_application():
-    database = "???"
-    host = "???"
-    user = "???"
-    password = "???"
-    port = "5432"
+    # database = input("Enter database name: ")
+    # host = input("Enter host name: ")
+    # user = input("Enter user name: ")
+    # password = input("Enter database name: ")
+    # port = "5432" # Default port
 
-    #connection = psycopg2.connect(database=database, host=host, user=user, password=password, port=port)
+    # connection = psycopg2.connect(database=database, host=host, user=user, password=password, port=port) # Connecting to PostgreSQL database
+    # cursor = connection.cursor() # Cursor to interact with the database
 
-    #cursor = connection.cursor() # Create a new cursor to interact with the PostgreSQL database
+    # cursor.execute("SELECT * FROM students") # Test
+    # print(cursor.fetchall()) # Test
 
-    #cursor.execute("SELECT * FROM DB_table WHERE id = 1")
-
-    #print(cursor.fetchall()) # Test output
-
-    while(True): # Necessary or not???
-        print("1. Get all students")
+    while(True):
+        print("\n1. Get all students")
         print("2. Add a student")
         print("3. Update student email")
         print("4. Delete student record")
+        print("5. Exit")
         option = input("Enter choice: ")
 
         try:
             option = int(option)
         except ValueError:
             print("Incorrect input.")
+            continue
 
         if option == 1:
             getAllStudents()
         elif option == 2:
-            first_name = input("Enter first name")
+            first_name = input("Enter first name: ")
             first_name = first_name[:CHAR_LIMIT] if len(first_name) > CHAR_LIMIT else first_name # Reducing to 255 characters
 
-            last_name = input("Enter last name")
+            last_name = input("Enter last name: ")
             last_name = last_name[:CHAR_LIMIT] if len(last_name) > CHAR_LIMIT else last_name
 
-            email = input("Enter email")
+            email = input("Enter email: ")
             email = email[:CHAR_LIMIT] if len(email) > CHAR_LIMIT else email
 
-            enrollment_date = datetime.now() # Change this later
+            enrollment_date = input("Enter enrollment date in the form year-month-day: ") # Error checking needed here?
 
             addStudent(first_name, last_name, email, enrollment_date)
+        elif option == 3:
+            student_id = input("Enter student ID: ")
+
+            new_email = input("Enter email: ")
+            new_email = new_email[:CHAR_LIMIT] if len(new_email) > CHAR_LIMIT else new_email
+
+            updateStudentEmail(student_id, new_email)
+        elif option == 4:
+            deleteStudent(input("Enter student ID: "))
+        elif option == 5:
+            exit(1)
+        else:
+            print("Incorrect input.")
+
+def end_application(): # Just to close the database connection properly
+    cursor.close()
+    connection.close()
 
 def getAllStudents():
     """
     Retrieves and displays all records from the students table.
     """
-    pass
+    cursor.execute("SELECT * FROM students") # All records in the table
+
+    column_names = [description[0] for description in cursor.description] # Retrieves each column name
+
+    print("-----------------------------------------------------------------") 
+    for column in column_names:
+        print(f"| {column} ", end="")
+    print("|")
+    print("-----------------------------------------------------------------")
+
+    rows = [row for row in cursor] # Get all row data
+    for row in rows:
+        print("|", end="")
+        for column in row: # Each individual attribute for each student
+            print(f" {column} ", end="|")
+        print()
+    print("-----------------------------------------------------------------") 
 
 def addStudent(first_name, last_name, email, enrollment_date):
     """
     Adds the given data to a new row, then inserts the row into the students table.
 
     Arguments:
-        first_name:
-        last_name:
-        email:
-        enrollment_date: A datetime object?
+        first_name: String
+        last_name: String
+        email: String
+        enrollment_date: String
     """
-    pass
+
+    values = (first_name, last_name, email, enrollment_date) # New row, these get substituted into the query
+
+    cursor.execute("INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES (%s, %s, %s, %s);", values) 
+    connection.commit() # Completes the transaction, sending the query to the database
+    # psycopg2 should automatically stop the transaction if there are insertion errors
 
 def updateStudentEmail(student_id, new_email):
     """
     Updates the email address for a student with the specified student_id.
 
     Arguments:
-        student_id: int
+        student_id: String
         new_email: String
     """
-    pass
+
+    values = (new_email, student_id) # These get substituted into the query
+
+    cursor.execute("UPDATE students SET email = %s WHERE student_id = %s", values) # Updates the corresponding student's email
+    connection.commit() # If there is no student_id which matches, nothing gets updated, no harm done
 
 def deleteStudent(student_id):
     """
     Deletes the record of the student with the specified student_id
 
     Arguments:
-        student_id: int
+        student_id: String
     """
-    pass
+
+    cursor.execute("DELETE FROM students WHERE student_id = %s", student_id)
+    connection.commit() # If there is no student_id which matches, nothing gets deleted, no harm done
 
 if __name__ == "__main__":
     start_application()
+    end_application()
